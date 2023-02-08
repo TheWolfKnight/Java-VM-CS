cefee0c9c63601ba428f36b92b0f4a77d5334bb5
Philip Knudsen <philipalexknudsen@gmail.com>
2023-02-08 11:20:36 +0100

    removed TypeTag.cs changed the two to fields in VarType.cs To a tuple of string, stirng

--- src/Validator/Type/Models/VarType.cs
+++ src/Validator/Type/Models/VarType.cs
@@ -1,21 +1,20 @@
 
-using CS_Java_VM.Src.Validator.Type.Constants;
+using CS_Java_VM.Src.Validator.Type;
 
 namespace CS_Java_VM.Src.Validator.Type.Models;
 
-public class VarType {
-  public string VarName;
-  public E_TypeTags TypeTag;
-  public VarType? Inner;
 
-  public VarType(string varName, E_TypeTags typeTag, VarType? inner=null) {
-    VarName = varName;
-    TypeTag = typeTag;
+public class VarType {
+  public (string TypeTag, string VarName) Identity;
+  public VarType[]? Inner;
+
+  public VarType(string varName, string typeTag, VarType[]? inner=null) {
+    Identity = (typeTag, varName);
     Inner = inner;
   }
 
   public override string ToString()
   {
-    return $"VarType(VarName={VarName}, TypeTag={TypeTag})";
+    return $"VarType(VarName={Identity.VarName}, TypeTag={Identity.TypeTag})";
   }
 }
--- src/Validator/Type/TypeValidator.cs
+++ src/Validator/Type/TypeValidator.cs
@@ -10,8 +10,51 @@ public class TypeValidator {
   public Stack<VarType> CallStack;
   public List<Stack<VarType>> Heap;
 
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <param name="">  </param>
   public TypeValidator(ref JavaClass jc) {
     CallStack = new Stack<VarType>();
     Heap = new List<Stack<VarType>>();
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <param name="">  </param>
+  public void PushToCallStack(VarType varType) {
+    CallStack.Push(varType);
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  public VarType PopFromCallStack() {
+    return CallStack.Pop();
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  public void PushScopeToHeap() {
+    Heap.Append(new Stack<VarType>());
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <param name="">  </param>
+  public void PushVarTypeToHeap(VarType varType) {
+    Heap[^1].Push(varType);
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  public Stack<VarType> PopScopeFromHeap() {
+    Stack<VarType> result = Heap[^1];
+    Heap.RemoveAt(Heap.Count-1);
+    return result;
   }
 }
